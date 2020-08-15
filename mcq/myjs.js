eedc8cbe3f99a5d0e504de1957c093e28798ed7e
ap-calc <62733554+log1p@users.noreply.github.com>
2020-08-15 16:15:18 -0400

    u

---
+++ mcq/myjs.js
@@ -0,0 +1,42 @@
+
+var size = 300;
+var path ="dat/"
+var img="";
+var imga ="5a.png";
+var f = new Array(size);
+var fa = new Array(size);
+for(var i = 0; i <= size; i++) {
+  f[i] = i+1+".png";
+  //fa[i] =i+1+"a.png";
+}
+var fa = {"A", "B","C","A", "B","C","A", "B","C","A", "B","C","A", "B","C"}
+function getRandomImage(imagesArray, path) {
+   
+    path = path || 'dat/'; // default path here
+    
+    var i = Math.floor( Math.random() * imagesArray.length );
+    
+       img = imagesArray[ i ];
+       imga = fa[ i ];
+    
+
+
+      setupAnswer();
+
+     w3.hide('#cardanswer');
+
+    return '<img src="' + path + img + '" alt ="Mouse Over to Flip">';
+    
+}
+
+
+
+function setupAnswer(){
+  
+//document.getElementById('cardanswer').innerHTML='<img src="' + path + imga + '">';  
+document.getElementById('cardanswer').innerHTML=""'<center>' + imga + '</center>'"";  
+
+
+}
+
+
