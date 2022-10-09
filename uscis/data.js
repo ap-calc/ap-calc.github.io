d5eb73a1561669367998d6e8789295e4fc02f8a7
ap-calc <62735195+ap-calc@users.noreply.github.com>
2022-10-09 01:40:35 -0400

    d

---
+++ uscis/data.js
@@ -0,0 +1,108 @@
+
+ text = '{"bank" : [' +
+    '{ "id" : 0, "question" : "What is the supreme law of the land?", "answers" : "the Constitution" },'+
+    '{ "id" : 1, "question" : "What does the Constitution do?", "answers" : "sets up the government, defines the government, protects basic rights of Americans" },'+
+    '{ "id" : 2, "question" : "The idea of self-government is in the first three words of the Constitution. What are these words?", "answers" : "We the People" },'+
+    '{ "id" : 3, "question" : "What is an amendment?", "answers" : "a change (to the Constitution), an addition (to the Constitution)" },'+
+    '{ "id" : 4, "question" : "What do we call the first ten amendments to the Constitution?", "answers" : "the Bill of Rights" },'+
+    '{ "id" : 5, "question" : "What is one right or freedom from the First Amendment?", "answers" : "speech, religion, assembly, press, petition the government" },'+
+    '{ "id" : 6, "question" : "How many amendments does the Constitution have?", "answers" : "twenty-seven (27)" },'+
+    '{ "id" : 7, "question" : "What did the Declaration of Independence do?", "answers" : "announced our independence (from Great Britain),declared our independence (from Great Britain),said that the United States is free (from Great Britain)" },'+
+    '{ "id" : 8, "question" : "What are two rights in the Declaration of Independence?", "answers" : "life, liberty, pursuit of happiness"},'+
+    '{ "id" : 9, "question" : "What is freedom of religion?", "answers" : "You can practice any religion, or not practice a religion." },'+
+    '{ "id" : 10, "question" : "What is the economic system in the United States?", "answers" : "capitalist economy, market economy" },'+
+    '{ "id" : 11, "question" : "What is the rule of law", "answers" : "Everyone must follow the law. Leaders must of obey the law. Government must obey the law. No one is above the law." },'+
+    '{ "id" : 12, "question" : "Name one branch or part of government.", "answers" : "Congress. Legislative. President. Executive. The courts. Judicial."},'+
+    '{ "id" : 13, "question" : "What stops one branch of government from becoming too powerful?", "answers" : "checks and balances. separation of powers." },'+
+        '{ "id" : 14, "question" : "Who is in charge of the executive branch?", "answers" : "The President." },'+
+    '{ "id" : 15, "question" : "Who makes federal laws?", "answers" : "Congress. Senate and House (of Representatives). (U.S. or national) legislature." },'+
+    '{ "id" : 16, "question" : "What are the two parts of the U.S. Congress?", "answers" : "the Senate and House (of Representatives)" },'+
+    '{ "id" : 17, "question" : "How many U.S. Senators are there?", "answers" : "one hundred." },'+
+    '{ "id" : 18, "question" : "We elect a U.S. Senator for how many years?", "answers" : "Six." },'+
+    '{ "id" : 19, "question" : "Who is one of the U.S. Senators in your state now?", "answers" : "(answer varies)" },'+
+    '{ "id" : 20, "question" : "The House of Representatives has how many voting members?", "answers" : "four hundred thirty-five" },'+
+    '{ "id" : 21, "question" : "We elect a U.S. Representative for how many years?", "answers" : "Two. " },'+
+    '{ "id" : 22, "question" : "Name your U.S. Representative.", "answers" : "(answer varies)" },'+
+    '{ "id" : 23, "question" : "Who does a U.S. Senator represent?", "answers" : "all people of the state." },'+
+        '{ "id" : 24, "question" : "Why do some states have more Representatives than other states?", "answers" : "(because of) the state population. (because) they have more people. (because) some states have more people." },'+
+    '{ "id" : 25, "question" : "We elect a President for how many years?", "answers" : "Four." },'+
+    '{ "id" : 26, "question" : "In what month do we vote for President?", "answers" : "November." },'+
+    '{ "id" : 27, "question" : "What is the name of the President of the United States now?", "answers" : "(the answer varies)" },'+
+    '{ "id" : 28, "question" : "What is the name of the Vice President of the United States now?", "answers" : "(the answer varies)" },'+
+    '{ "id" : 29, "question" : "If the President can no longer serve, who becomes President?", "answers" : "The Vice President." },'+
+    '{ "id" : 30, "question" : "If both the President and the Vice President can no longer serve, who becomes President?", "answers" : "The Speaker of the House." },'+
+    '{ "id" : 31, "question" : "Who is the Commander in Chief of the military?", "answers" : "The President." },'+
+    '{ "id" : 32, "question" : "Who signs bills to become laws?", "answers" : "The President." },'+
+    '{ "id" : 33, "question" : "Who vetoes bills?", "answers" : "The President." },'+
+        '{ "id" : 34, "question" : "What does the Presidential Cabinet do? ", "answers" : "Advises the President." },'+
+    '{ "id" : 35, "question" : "What are two Cabinet-level positions?", "answers" : "Secretary of Agriculture. Secretary of Commerce. Secretary of Defense. Secretary of Education. Secretary of Energy. Secretary of Health and Human Services. Secretary of Homeland Security. Secretary of Housing and Urban Development. Secretary of the Interior. Secretary of Labor. Secretary of State. Secretary of Transportation. Secretary of the Treasury. Secretary of Veterans Affairs. Attorney General. Vice President." },'+
+    '{ "id" : 36, "question" : "What does the judicial branch do?", "answers" : "reviews laws. explains laws. resolves disputes (disagreements). decides if a law goes against the Constitution." },'+
+    '{ "id" : 37, "question" : "What is the highest court in the United States?", "answers" : "The Supreme Court" },'+
+    '{ "id" : 38, "question" : "How many justices are on the Supreme Court?", "answers" : "Nine." },'+
+    '{ "id" : 39, "question" : "Under our Constitution, some powers belong to the federal government. What is one power of the federal government?", "answers" : "to print money. to declare war. to create an army. to make treaties." },'+
+    '{ "id" : 40, "question" : "Who is the Chief Justice of the United States now?", "answers" : "(Justice John) Roberts" },'+
+    '{ "id" : 41, "question" : "Under our Constitution, some powers belong to the states. What is one power of the states? ", "answers" : "provide schooling and education. provide protection (police). provide safety (fire departments). give driver licenses. approve zoning and land use.." },'+
+    '{ "id" : 42, "question" : "Who is the Governor of your state now?", "answers" : "(answer varies)" },'+
+    '{ "id" : 43, "question" : "What is the capital of your state?", "answers" : "(answer varies)" },'+
+    '{ "id" : 44, "question" : "What are the two major political parties in the United States?", "answers" : "Democratic and Republican." },'+
+    '{ "id" : 45, "question" : "What is the political party of the President now?", "answers" : "(answer varies)" },'+
+    '{ "id" : 46, "question" : "What is the name of the Speaker of the House of Representatives now?", "answers" : "(answer varies)" },'+
+    '{ "id" : 47, "question" : "There are four amendments to the Constitution about who can vote. Describe one of them.", "answers" : "Citizens eighteen (18) and older (can vote). You do not have to pay (a poll tax) to vote. Any citizen can vote. (Women and men can vote.) . A male citizen of any race (can vote)." },'+
+    '{ "id" : 48, "question" : "What is one responsibility that is only for United States citizens? ", "answers" : "serve on a jury. vote in a federal election." },'+
+    '{ "id" : 49, "question" : "Name one right only for United States citizens.", "answers" : "vote in a federal election. run for federal office." },'+
+    '{ "id" : 50, "question" : "What are two rights of everyone living in the United States?", "answers" : "freedom of expression. freedom of speech. freedom of assembly. freedom to petition the government. freedom of religion. the right to bear arms." },'+
+    '{ "id" : 51, "question" : "What do we show loyalty to when we say the Pledge of Allegiance?", "answers" : "the United States. the flag." },'+
+    '{ "id" : 52, "question" : "What is one promise you make when you become a United States citizen?", "answers" : "give up loyalty to other countries. defend the Constitution and laws of the United States. obey the laws of the United States. serve in the U.S. military (if needed). serve (do important work for) the nation (if needed). be loyal to the United States." },'+
+    '{ "id" : 53, "question" : "How old do citizens have to be to vote for President?", "answers" : "eighteen (18) and older" },'+
+    '{ "id" : 54, "question" : "What are two ways that Americans can participate in their democracy?", "answers" : "vote. join a political party. help with a campaign. join a civic group. join a community group. give an elected official your opinion on an issue. call Senators and Representatives. publicly support or oppose an issue or policy. run for office. write to a newspaper." },'+
+    '{ "id" : 55, "question" : "When is the last day you can send in federal income tax forms?", "answers" : "April 15" },'+
+    '{ "id" : 56, "question" : "When must all men register for the Selective Service?", "answers" : "at age eighteen (18). between eighteen (18) and twenty-six (26)." },'+
+    '{ "id" : 57, "question" : "What is one reason colonists came to America?", "answers" : "freedom. political liberty. religious freedom. economic opportunity. practice their religion. escape persecution." },'+
+    '{ "id" : 58, "question" : "Who lived in America before the Europeans arrived?", "answers" : "American Indians. Native Americans." },'+
+    '{ "id" : 59, "question" : "What group of people was taken to America and sold as slaves?", "answers" : "Africans. People from Africa." },'+
+    '{ "id" : 60, "question" : "Why did the colonists fight the British?", "answers" : "Because of high taxes (taxation without representation). Because the British army stayed in their houses (boarding, quartering). Because they did not have self-government." },'+
+    '{ "id" : 61, "question" : "Who wrote the Declaration of Independence?", "answers" : "(Thomas) Jefferson" },'+
+    '{ "id" : 62, "question" : "When was the Declaration of Independence adopted?", "answers" : "July 4, 1776." },'+
+    '{ "id" : 63, "question" : "There were 13 original states. Name three.", "answers" : "New Hampshire. Massachusetts. Rhode Island. Connecticut. New York. New Jersey. Pennsylvania. Delaware. Maryland. Virginia. North Carolina. South Carolina. Georgia." },'+
+    '{ "id" : 64, "question" : "What happened at the Constitutional Convention?", "answers" : "The Constitution was written. The Founding Fathers wrote the Constitution." },'+
+    '{ "id" : 65, "question" : "When was the Constitution written?", "answers" : "1787" },'+
+    '{ "id" : 66, "question" : "The Federalist Papers supported the passage of the U.S. Constitution. Name one of the writers.", "answers" : "(James) Madison. (Alexander) Hamilton. (John) Jay. Publius." },'+
+    '{ "id" : 67, "question" : "What is one thing Benjamin Franklin is famous for?", "answers" : "U.S. diplomat. Oldest member of the Constitutional Convention. First Postmaster General of the United States. Writer of Poor Richards Almanac. Started the first free libraries." },'+
+    '{ "id" : 68, "question" : "Who is the Father of Our Country?", "answers" : "(George) Washington" },'+
+    '{ "id" : 69, "question" : "Who was the first President?", "answers" : "(George) Washington." },'+
+    '{ "id" : 70, "question" : "What territory did the United States buy from France in 1803?", "answers" : "The Louisiana Territory. Louisiana." },'+
+    '{ "id" : 71, "question" : "Name one war fought by the United States in the 1800s.", "answers" : "War of 1812. Mexican-American War. Civil War. Spanish-American War." },'+
+    '{ "id" : 72, "question" : "Name the U.S. war between the North and the South.", "answers" : "The Civil War. The War between the States " },'+
+    '{ "id" : 73, "question" : "Name one problem that led to the Civil War.", "answers" : "Slavery . Economic reasons. States rights." },'+
+    '{ "id" : 74, "question" : "What was one important thing that Abraham Lincoln did?", "answers" : "Freed the slaves (Emancipation Proclamation). Saved (or preserved) the Union. Led the United States during the Civil War." },'+
+    '{ "id" : 75, "question" : "What did the Emancipation Proclamation do?", "answers" : "Freed the slaves. Freed slaves in the Confederacy. Freed slaves in the Confederate states. Freed slaves in most Southern states." },'+
+    '{ "id" : 76, "question" : "What did Susan B. Anthony do?  ", "answers" : "Fought for womens rights. Fought for civil rights." },'+
+    '{ "id" : 77, "question" : "Name one war fought by the United States in the 1900s.", "answers" : "World War I. World War II. Korean War. Vietnam War. (Persian) Gulf War." },'+
+    '{ "id" : 78, "question" : "Who was President during World War I?", "answers" : "(Woodrow) Wilson" },'+
+    '{ "id" : 79, "question" : "Who was President during the Great Depression and World War II?", "answers" : "(Franklin) Roosevelt." },'+
+    '{ "id" : 80, "question" : "Who did the United States fight in World War II?", "answers" : "Japan, Germany, Italy." },'+
+    '{ "id" : 81, "question" : "Before he was President, Eisenhower was a general. What war was he in?", "answers" : "World War II." },'+
+    '{ "id" : 82, "question" : "During the Cold War, what was the main concern of the United States?", "answers" : "communism" },'+
+    '{ "id" : 83, "question" : "What movement tried to end racial discrimination?", "answers" : "civil rights (movement)" },'+
+    '{ "id" : 84, "question" : "What did Martin Luther King, Jr. do?", "answers" : "Fought for civil rights. Worked for equality for all Americans." },'+
+    '{ "id" : 85, "question" : "What major event happened on September 11, 2001, in the United States?", "answers" : "Terrorists attacked the United States." },'+
+    '{ "id" : 86, "question" : "Name one American Indian tribe in the United States.", "answers" : "Cherokee. Navajo. Sioux. Chippewa. Choctaw. Pueblo. Apache. Iroquois. Creek. Blackfeet. Seminole. Cheyenne. Arawak. Shawnee. Mohegan. Huron. Oneida. Lakota. Crow. Teton. Hopi. Inuit." },'+
+    '{ "id" : 87, "question" : "Name one of the two longest rivers in the United States.", "answers" : "Missouri (River). Mississippi (River)" },'+
+    '{ "id" : 88, "question" : "What ocean is on the West Coast of the United States? ", "answers" : "Pacific (Ocean)" },'+
+    '{ "id" : 89, "question" : "What ocean is on the East Coast of the United States?", "answers" : "Atlantic (Ocean)." },'+
+    '{ "id" : 90, "question" : "Name one U.S. territory.", "answers" : "Puerto Rico. U.S. Virgin Islands. American Samoa. Northern Mariana Islands. Guam." },'+
+    '{ "id" : 91, "question" : "Name one state that borders Canada.", "answers" : "Maine. New Hampshire. Vermont. New York. Pennsylvania. Ohio. Michigan. Minnesota. North Dakota. Montana. Idaho. Washington. Alaska." },'+
+    '{ "id" : 92, "question" : "Name one state that borders Mexico.", "answers" : "California. Arizona. New Mexico. Texas." },'+
+    '{ "id" : 93, "question" : "What is the capital of the United States?", "answers" : " Washington, D.C." },'+
+    '{ "id" : 94, "question" : "Where is the Statue of Liberty?", "answers" : "New York (Harbor). Liberty Island. Also acceptable are New Jersey, near New York City, and on the Hudson (River)." },'+
+    '{ "id" : 95, "question" : "Why does the flag have 13 stripes?", "answers" : "Because there were 13 original colonies. Because the stripes represent the original colonies." },'+
+    '{ "id" : 96, "question" : "Why does the flag have 50 stars?", "answers" : "Because there is one star for each state. Because each star represents a state. Because there are 50 states." },'+
+    '{ "id" : 97, "question" : "What is the name of the national anthem?", "answers" : " The Star-Spangled Banner." },'+
+    '{ "id" : 98, "question" : "When do we celebrate Independence Day?", "answers" : "July 4" },'+
+    '{ "id" : 99, "question" : "Name two national US holidays.", "answers" : "New Years Day Martin Luther King Jr. Day. Presidents Day. Memorial Day. Independence Day. Labor Day. Columbus Day. Veterans Day. Thanksgiving. Christmas."} ]}';
+
+const obj = JSON.parse(text);
+
+ 
+
+ 
