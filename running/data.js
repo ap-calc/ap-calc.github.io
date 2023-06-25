46a06408cc85e901305d5b38017ea697d5aa8500
ap-calc <62735195+ap-calc@users.noreply.github.com>
2023-06-25 19:54:27 -0400

    d

---
+++ running/data.js
@@ -0,0 +1,148 @@
+const mydata = [
+    {
+      "year": 2021,
+      "month": "Jul",
+      "distance": 125.76,
+      "time": 1070.61
+    },
+    {
+      "year": 2021,
+      "month": "Aug",
+      "distance": 134.27,
+      "time": 1171.32
+    },
+    {
+      "year": 2021,
+      "month": "Sep",
+      "distance": 138.3,
+      "time": 1164.98
+    },
+    {
+      "year": 2021,
+      "month": "Oct",
+      "distance": 132.07,
+      "time": 1065.69
+    },
+    {
+      "year": 2021,
+      "month": "Nov",
+      "distance": 107.49,
+      "time": 865.73
+    },
+    {
+      "year": 2021,
+      "month": "Dec",
+      "distance": 72.6,
+      "time": 578.54
+    },
+    {
+      "year": 2022,
+      "month": "Jan",
+      "distance": 75.58,
+      "time": 623.7
+    },
+    {
+      "year": 2022,
+      "month": "Feb",
+      "distance": 61.16,
+      "time": 507.44
+    },
+    {
+      "year": 2022,
+      "month": "Mar",
+      "distance": 126.03,
+      "time": 1032.81
+    },
+    {
+      "year": 2022,
+      "month": "Apr",
+      "distance": 114.97,
+      "time": 924.1
+    },
+    {
+      "year": 2022,
+      "month": "May",
+      "distance": 121.48,
+      "time": 980.85
+    },
+    {
+      "year": 2022,
+      "month": "Jun",
+      "distance": 158.23,
+      "time": 1315.63
+    },
+    {
+      "year": 2022,
+      "month": "Jul",
+      "distance": 185.49,
+      "time": 1533.26
+    },
+    {
+      "year": 2022,
+      "month": "Aug",
+      "distance": 175.85,
+      "time": 1472.91
+    },
+    {
+      "year": 2022,
+      "month": "Sep",
+      "distance": 157.02,
+      "time": 1318.5
+    },
+    {
+      "year": 2022,
+      "month": "Oct",
+      "distance": 130.58,
+      "time": 1166.67
+    },
+    {
+      "year": 2022,
+      "month": "Nov",
+      "distance": 127.99,
+      "time": 1110.82
+    },
+    {
+      "year": 2022,
+      "month": "Dec",
+      "distance": 66.86,
+      "time": 574.75
+    },
+    {
+      "year": 2023,
+      "month": "Jan",
+      "distance": 79.26,
+      "time": 669.64
+    },
+    {
+      "year": 2023,
+      "month": "Feb",
+      "distance": 65.71,
+      "time": 547.69
+    },
+    {
+      "year": 2023,
+      "month": "Mar",
+      "distance": 133.46,
+      "time": 1125.06
+    },
+    {
+      "year": 2023,
+      "month": "Apr",
+      "distance": 147.96,
+      "time": 1227.83
+    },
+    {
+      "year": 2023,
+      "month": "May",
+      "distance": 166.47,
+      "time": 1348.23
+    },
+    {
+      "year": 2023,
+      "month": "Jun",
+      "distance": 110.89,
+      "time": 945.48
+    }
+  ];
+
+  console.log(mydata[0].distance)
