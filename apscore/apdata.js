e1dec5aca17e39510b2898845c91a98c31b30c7f
ap-calc <62735195+ap-calc@users.noreply.github.com>
2023-06-30 16:53:12 -0400

    Add files via upload

---
+++ apscore/apdata.js
@@ -0,0 +1,172 @@
+const abdata= [
+    {
+      "year": 2006,
+      "one": 4,
+      "two": 1,
+      "three": 4,
+      "four": 1,
+      "five": 3,
+      "total": 13,
+      "avg": 2.846
+    },
+    {
+      "year": 2007,
+      "one": 0,
+      "two": 1,
+      "three": 1,
+      "four": 2,
+      "five": 7,
+      "total": 11,
+      "avg": 4.364
+    },
+    {
+      "year": 2008,
+      "one": 1,
+      "two": 0,
+      "three": 2,
+      "four": 4,
+      "five": 6,
+      "total": 13,
+      "avg": 4.077
+    },
+    {
+      "year": 2009,
+      "one": 0,
+      "two": 1,
+      "three": 1,
+      "four": 1,
+      "five": 13,
+      "total": 16,
+      "avg": 4.625
+    },
+    {
+      "year": 2010,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 1,
+      "five": 7,
+      "total": 8,
+      "avg": 4.875
+    },
+    {
+      "year": 2011,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 2,
+      "five": 5,
+      "total": 7,
+      "avg": 4.714
+    },
+    {
+      "year": 2012,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 3,
+      "five": 7,
+      "total": 10,
+      "avg": 4.7
+    },
+    {
+      "year": 2013,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 1,
+      "five": 12,
+      "total": 13,
+      "avg": 4.923
+    },
+    {
+      "year": 2014,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 0,
+      "five": 6,
+      "total": 6,
+      "avg": 5
+    },
+    {
+      "year": 2015,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 1,
+      "five": 6,
+      "total": 7,
+      "avg": 4.857
+    },
+    {
+      "year": 2016,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 1,
+      "five": 15,
+      "total": 16,
+      "avg": 4.938
+    },
+    {
+      "year": 2017,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 5,
+      "five": 4,
+      "total": 9,
+      "avg": 4.444
+    },
+    {
+      "year": 2018,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 3,
+      "five": 6,
+      "total": 9,
+      "avg": 4.667
+    },
+    {
+      "year": 2019,
+      "one": 0,
+      "two": 1,
+      "three": 0,
+      "four": 0,
+      "five": 11,
+      "total": 12,
+      "avg": 4.75
+    },
+    {
+      "year": 2020,
+      "one": 0,
+      "two": 1,
+      "three": 2,
+      "four": 1,
+      "five": 5,
+      "total": 9,
+      "avg": 4.111
+    },
+    {
+      "year": 2021,
+      "one": 0,
+      "two": 1,
+      "three": 3,
+      "four": 1,
+      "five": 5,
+      "total": 10,
+      "avg": 4
+    },
+    {
+      "year": 2022,
+      "one": 0,
+      "two": 0,
+      "three": 1,
+      "four": 0,
+      "five": 3,
+      "total": 4,
+      "avg": 4.5
+    }
+];
