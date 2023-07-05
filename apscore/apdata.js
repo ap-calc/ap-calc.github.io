12d1884e9ee6df6ec14f04330b82432f663e22d6
ap-calc <62735195+ap-calc@users.noreply.github.com>
2023-07-05 10:25:39 -0400

    s

---
+++ apscore/apdata.js
@@ -0,0 +1,381 @@
+const abdata= [
+    {
+      "year": 2006,
+      "one": 4,
+      "two": 1,
+      "three": 4,
+      "four": 1,
+      "five": 3,
+      "total": 13,
+      "avg": 2.846
+    },
+    {
+      "year": 2007,
+      "one": 0,
+      "two": 1,
+      "three": 1,
+      "four": 2,
+      "five": 7,
+      "total": 11,
+      "avg": 4.364
+    },
+    {
+      "year": 2008,
+      "one": 1,
+      "two": 0,
+      "three": 2,
+      "four": 4,
+      "five": 6,
+      "total": 13,
+      "avg": 4.077
+    },
+    {
+      "year": 2009,
+      "one": 0,
+      "two": 1,
+      "three": 1,
+      "four": 1,
+      "five": 13,
+      "total": 16,
+      "avg": 4.625
+    },
+    {
+      "year": 2010,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 1,
+      "five": 7,
+      "total": 8,
+      "avg": 4.875
+    },
+    {
+      "year": 2011,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 2,
+      "five": 5,
+      "total": 7,
+      "avg": 4.714
+    },
+    {
+      "year": 2012,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 3,
+      "five": 7,
+      "total": 10,
+      "avg": 4.7
+    },
+    {
+      "year": 2013,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 1,
+      "five": 12,
+      "total": 13,
+      "avg": 4.923
+    },
+    {
+      "year": 2014,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 0,
+      "five": 6,
+      "total": 6,
+      "avg": 5
+    },
+    {
+      "year": 2015,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 1,
+      "five": 6,
+      "total": 7,
+      "avg": 4.857
+    },
+    {
+      "year": 2016,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 1,
+      "five": 15,
+      "total": 16,
+      "avg": 4.938
+    },
+    {
+      "year": 2017,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 5,
+      "five": 4,
+      "total": 9,
+      "avg": 4.444
+    },
+    {
+      "year": 2018,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 3,
+      "five": 6,
+      "total": 9,
+      "avg": 4.667
+    },
+    {
+      "year": 2019,
+      "one": 0,
+      "two": 1,
+      "three": 0,
+      "four": 0,
+      "five": 11,
+      "total": 12,
+      "avg": 4.75
+    },
+    {
+      "year": 2020,
+      "one": 0,
+      "two": 1,
+      "three": 2,
+      "four": 1,
+      "five": 5,
+      "total": 9,
+      "avg": 4.111
+    },
+    {
+      "year": 2021,
+      "one": 0,
+      "two": 1,
+      "three": 3,
+      "four": 1,
+      "five": 5,
+      "total": 10,
+      "avg": 4
+    },
+    {
+      "year": 2022,
+      "one": 0,
+      "two": 0,
+      "three": 1,
+      "four": 0,
+      "five": 3,
+      "total": 4,
+      "avg": 4.5
+    },
+     {
+      "year": 2023,
+      "one": 0,
+      "two": 0,
+      "three": 0,
+      "four": 0,
+      "five": 3,
+      "total": 3,
+      "avg": 5.0
+    }];
+
+
+const bcdata = [
+  {
+    "year": 2006,
+    "one": 0,
+    "two": 0,
+    "three": 0,
+    "four": 0,
+    "five": 1,
+    "total": 1,
+    "avg": 5
+  },
+  {
+    "year": 2007,
+    "one": 1,
+    "two": 0,
+    "three": 1,
+    "four": 0,
+    "five": 1,
+    "total": 3,
+    "avg": 3
+  },
+  {
+    "year": 2008,
+    "one": 0,
+    "two": 0,
+    "three": 4,
+    "four": 3,
+    "five": 6,
+    "total": 13,
+    "avg": 4.154
+  },
+  {
+    "year": 2009,
+    "one": 0,
+    "two": 0,
+    "three": 1,
+    "four": 0,
+    "five": 11,
+    "total": 12,
+    "avg": 4.833
+  },
+  {
+    "year": 2010,
+    "one": 0,
+    "two": 0,
+    "three": 1,
+    "four": 5,
+    "five": 25,
+    "total": 31,
+    "avg": 4.774
+  },
+  {
+    "year": 2011,
+    "one": 0,
+    "two": 0,
+    "three": 2,
+    "four": 2,
+    "five": 14,
+    "total": 18,
+    "avg": 4.667
+  },
+  {
+    "year": 2012,
+    "one": 0,
+    "two": 0,
+    "three": 0,
+    "four": 1,
+    "five": 10,
+    "total": 11,
+    "avg": 4.909
+  },
+  {
+    "year": 2013,
+    "one": 0,
+    "two": 0,
+    "three": 0,
+    "four": 2,
+    "five": 13,
+    "total": 15,
+    "avg": 4.867
+  },
+  {
+    "year": 2014,
+    "one": 0,
+    "two": 0,
+    "three": 1,
+    "four": 2,
+    "five": 16,
+    "total": 19,
+    "avg": 4.789
+  },
+  {
+    "year": 2015,
+    "one": 0,
+    "two": 1,
+    "three": 1,
+    "four": 1,
+    "five": 13,
+    "total": 16,
+    "avg": 4.625
+  },
+  {
+    "year": 2016,
+    "one": 0,
+    "two": 1,
+    "three": 0,
+    "four": 0,
+    "five": 14,
+    "total": 15,
+    "avg": 4.8
+  },
+  {
+    "year": 2017,
+    "one": 0,
+    "two": 0,
+    "three": 1,
+    "four": 1,
+    "five": 7,
+    "total": 9,
+    "avg": 4.667
+  },
+  {
+    "year": 2018,
+    "one": 0,
+    "two": 0,
+    "three": 0,
+    "four": 3,
+    "five": 11,
+    "total": 14,
+    "avg": 4.786
+  },
+  {
+    "year": 2019,
+    "one": 0,
+    "two": 2,
+    "three": 1,
+    "four": 1,
+    "five": 7,
+    "total": 11,
+    "avg": 4.182
+  },
+  {
+    "year": 2020,
+    "one": 0,
+    "two": 0,
+    "three": 1,
+    "four": 0,
+    "five": 4,
+    "total": 5,
+    "avg": 4.6
+  },
+  {
+    "year": 2021,
+    "one": 0,
+    "two": 1,
+    "three": 0,
+    "four": 0,
+    "five": 7,
+    "total": 8,
+    "avg": 4.625
+  },
+  {
+    "year": 2022,
+    "one": 0,
+    "two": 0,
+    "three": 0,
+    "four": 2,
+    "five": 1,
+    "total": 3,
+    "avg": 4.333
+  },
+    {   "year": 2023,
+    "one": 0,
+    "two": 0,
+    "three": 0,
+    "four": 1,
+    "five": 3,
+    "total": 4,
+    "avg": 4.75
+}];
+
+//create arrays
+const mydata_ab = [['year','1 - one','2 - two','3 - three','4 - four','5 - five']]
+const mydata_bc = [['year','1 - one','2 - two','3 - three','4 - four','5 - five']]
+const mydata = [['year','1 - one','2 - two','3 - three','4 - four','5 - five']]
+
+for (i = 0; i < abdata.length; i++) {
+
+    mydata_ab.push([abdata[i].year+"",abdata[i].one,abdata[i].two,abdata[i].three,abdata[i].four,abdata[i].five]);
+     
+}
+
+for (i = 0; i < bcdata.length; i++) {
+  mydata_bc.push([bcdata[i].year+"",bcdata[i].one,bcdata[i].two,bcdata[i].three,bcdata[i].four,bcdata[i].five]);
+  mydata.push([abdata[i].year+"",abdata[i].one + bcdata[i].one,abdata[i].two+bcdata[i].two,abdata[i].three+bcdata[i].three,abdata[i].four + bcdata[i].four,abdata[i].five + bcdata[i].five]);
+
+}
+
